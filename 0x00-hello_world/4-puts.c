d89299f9f0e98e26e7ef040b202e3a50b944074f
Kwame Addoh <kayaddoh@gmail.com>
2022-10-13 09:51:19 -0700

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,5 +5,5 @@
  */
 int main(void) 
 {
-        puts("\"Programming is just like building a multilingual puzzle");	        return (0);
+        puts("\"Programming is just like building a multilingual puzzle");      return (0);
 }	
