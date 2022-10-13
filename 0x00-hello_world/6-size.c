22c36fd95d63679aa0e5ec067aa9f428eec83720
Kwame Addoh <kayaddoh@gmail.com>
2022-10-13 15:47:56 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,6 @@
-
 #include <stdio.h>
 /**
- * main - prints the sizes of various types
+ * main - prints the sizes of various files
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
