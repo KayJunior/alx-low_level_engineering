a4c666c4cbb0b16495dfa5afc717da362e16522d
Kwame Addoh <kayaddoh@gmail.com>
2022-10-14 13:08:28 -0700

    Pos or Neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - assigns a random number to int n everytime
- * it executes, and prints it
+ * it executes and prints it
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,10 +13,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is postitive\n", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
+
